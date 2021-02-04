d7c6407028f1f8c178856853694a124c5015bc1d
Masoud Hassani <m.hasany@gmail.com>
2021-02-03 22:38:10 -0500

    refactored the package

---
+++ scripts/motor_driver_ros_wrapper.py
@@ -0,0 +1,77 @@
+#!/usr/bin/env python3
+
+import rospy
+from motor_pwm.motor_driver import MotorDriver
+
+from std_msgs.msg import Int16
+from std_srvs.srv import Trigger
+
+# the following are commented since they give error
+# from diagnostic_msgs.msg import DiagnosticStatus
+# from diagnostic_msgs.msg import KeyValue
+
+class MotorDriverROSWrapper:
+
+    def __init__(self):
+        max_speed = rospy.get_param("~max_speed", 100)
+        driver_pins = rospy.get_param("~driver_pins", [20, 21, 24])
+        pwm_freq = rospy.get_param("~pwm_freq", 200)
+
+        # publihing frequency
+        publish_current_speed_frequency = rospy.get_param("~publish_current_speed_frequency", 5.0)
+        # publish_motor_status_frequency = rospy.get_param("~publish_motor_status_frequency", 1.0)
+        
+        # initialize the motor
+        self.motor = MotorDriver(max_speed=max_speed)
+
+        # subscribe to motor speed command
+        rospy.Subscriber("speed_command", Int16, self.callback_speed_command)
+        
+        # initialize a service
+        rospy.Service("stop_motor", Trigger, self.callback_stop)
+
+        # set up publishers
+        self.current_speed_pub = rospy.Publisher("current_speed", Int16, queue_size=10)
+        # self.motor_status_pub = rospy.Publisher("motor_status", DiagnosticStatus, queue_size=1)
+
+        rospy.Timer(rospy.Duration(1.0/publish_current_speed_frequency), self.publish_current_speed)
+        # rospy.Timer(rospy.Duration(1.0/publish_motor_status_frequency), self.publish_motor_status)
+
+    '''
+    publish current motor speed
+    '''
+    def publish_current_speed(self, event=None):
+        self.current_speed_pub.publish(self.motor.get_speed())
+
+    '''
+    TODO: publish motor status
+    def publish_motor_status(self, event=None):
+        status = self.motor.get_status()
+        data_list = []
+        for key in status:
+            data_list.append(KeyValue(key, str(status[key])))
+
+        msg = DiagnosticStatus()
+        msg.values = data_list
+
+        self.motor_status_pub.publish(msg)
+    '''
+    
+    def stop(self):
+        self.motor.stop()
+
+    def callback_speed_command(self, msg):
+        self.motor.set_pwm(msg.data)
+        
+    def callback_stop(self, req):
+        self.stop()
+        return {"success": True, "message": "Motor has been stopped"}
+
+if __name__ == "__main__":
+    rospy.init_node("motor_driver")
+
+    motor_driver_wrapper = MotorDriverROSWrapper()
+    rospy.on_shutdown(motor_driver_wrapper.stop)
+
+    rospy.loginfo("Motor driver is now started, ready to get commands.")
+    rospy.spin()
---
+++ src/motor_pwm/motor_driver.py
@@ -0,0 +1,91 @@
+import RPi.GPIO as io
+
+class MotorDriver:
+    def __init__(self, max_speed=100, driver_pins=[20, 21, 24], pwm_freq=1000):
+        self.max_speed = max_speed   # 0~100% of pwm duty cycle
+        self.current_speed = 0
+        self.driver_pins = driver_pins    # [fwd, rev, pwm signal] 
+        self.pwm_freq = pwm_freq
+
+        # GPIO setting
+        io.setmode(io.BCM)     # bcm pin numbering
+        io.setwarnings(False)
+
+        # vnh driver with fwd, rev, and pwm signal pin
+        if len(driver_pins) == 3:
+            # set the GPIO modes 
+            io.setup(driver_pins[0], io.OUT)
+            io.setup(driver_pins[1], io.OUT)
+            io.setup(driver_pins[2], io.OUT)
+
+            # initialize pwm
+            self.motor_pwm = io.PWM(driver_pins[2], pwm_freq) # pin number, frequency
+            self.motor_pwm.start(0)  
+
+            # initialize direction pins
+            io.output(driver_pins[0], io.LOW)
+            io.output(driver_pins[1], io.LOW)
+
+        # drivers with 2 pwm pin as input such as TB9051FTG
+        elif len(driver_pins) == 2:
+            # set the GPIO modes 
+            io.setup(driver_pins[0], io.OUT)
+            io.setup(driver_pins[1], io.OUT)
+
+            # initialize pwm
+            fwd_pwm = io.PWM(driver_pins[0], pwm_freq)   # forward pwm signal
+            rev_pwm = io.PWM(driver_pins[1], pwm_freq)   # reverse pwm signal
+            fwd_pwm.start(0)
+            rev_pwm.start(0)
+
+        #TODO:a
+        self.voltage = 12
+        self.temperature = 47
+
+    # turn all motors off
+    def stop(self):
+        if len(self.driver_pins) == 3:
+            self.motor_pwm.ChangeDutyCycle(0)
+            io.output(self.driver_pins[0], io.LOW)
+            io.output(self.driver_pins[1], io.LOW)   
+
+    #  forward direction
+    def forward(self, duty_cycle):
+        if len(self.driver_pins) == 3:
+            self.motor_pwm.ChangeDutyCycle(duty_cycle)
+            io.output(self.driver_pins[0], io.HIGH)
+            io.output(self.driver_pins[1], io.LOW)
+
+    #  reverse direction
+    def reverse(self, duty_cycle):
+        self.motor_pwm.ChangeDutyCycle(duty_cycle)
+        io.output(self.driver_pins[0], io.LOW)
+        io.output(self.driver_pins[1], io.HIGH)
+
+    def set_pwm(self, command):
+        """
+        expects a command between -1 and 1 and converts it to duty cycle
+        """
+        command = min(max(command, -1), 1)
+        self.current_speed = command*self.max_speed
+
+        if command < 0:
+            self.reverse(self.current_speed)
+
+        else:
+            self.forward(command*self.max_speed)
+
+    def get_speed(self):
+        """
+        return current speed
+        """
+        return self.current_speed
+
+    def get_status(self):
+        """
+        TODO: get hardware information from the motor
+        """
+        return {
+            'temperature': self.temperature,
+            'voltage': self.voltage
+        }
